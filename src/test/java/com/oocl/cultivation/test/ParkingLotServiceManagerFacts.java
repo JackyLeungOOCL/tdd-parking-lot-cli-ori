1860027935cf1651aae4576d5c1806dbb62880be
jacky.leung <jacky.leung@oocl.com>
2018-11-25 22:36:31 +0800

    [LEUNGJA] parking lot service manager parks cars as a standard parking boy

--- src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerFacts.java
@@ -3,9 +3,10 @@ package com.oocl.cultivation.test;
 import com.oocl.cultivation.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotServiceManagerFacts {
     @Test
@@ -45,4 +46,67 @@ public class ParkingLotServiceManagerFacts {
         Throwable exception = assertThrows(RuntimeException.class, () -> manager.fetch(ticket, parkingBoy2));
         assertEquals("Parking boy is not managed by this manager.", exception.getMessage());
     }
+
+//    @Test
+//    void should_park_cars_as_a_standard_parking_boy() {
+//
+//    }
+    // Should park cars as a standard parking boy
+
+    @Test
+    void should_park_a_car_to_a_parking_lot_and_get_it_back() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLotServiceManager manager = new ParkingLotServiceManager(parkingLot);
+        Car car = new Car();
+
+        ParkingTicket ticket = manager.park(car);
+        Car fetched = manager.fetch(ticket);
+
+        assertSame(fetched, car);
+    }
+
+    @Test
+    void should_park_multiple_cars_to_a_parking_lot_and_get_them_back() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLotServiceManager manager = new ParkingLotServiceManager(parkingLot);
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        ParkingTicket firstTicket = manager.park(firstCar);
+        ParkingTicket secondTicket = manager.park(secondCar);
+
+        Car fetchedByFirstTicket = manager.fetch(firstTicket);
+        Car fetchedBySecondTicket = manager.fetch(secondTicket);
+
+        assertSame(firstCar, fetchedByFirstTicket);
+        assertSame(secondCar, fetchedBySecondTicket);
+    }
+
+    @Test
+    void should_park_cars_sequentially() {
+        final int capacity = 1;
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(capacity);
+        ParkingLot parkingLot2 = new ParkingLot(capacity);
+        ParkingLot parkingLot3 = new ParkingLot();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        ParkingLotServiceManager manager = new ParkingLotServiceManager(parkingLots);
+
+        manager.park(new Car());
+        assertEquals(parkingLot1.getAvailableParkingPosition(), 0);
+        assertEquals(parkingLot2.getAvailableParkingPosition(), 1);
+        assertEquals(parkingLot3.getAvailableParkingPosition(), 10);
+        manager.park(new Car());
+        assertEquals(parkingLot1.getAvailableParkingPosition(), 0);
+        assertEquals(parkingLot2.getAvailableParkingPosition(), 0);
+        assertEquals(parkingLot3.getAvailableParkingPosition(), 10);
+        manager.park(new Car());
+        assertEquals(parkingLot1.getAvailableParkingPosition(), 0);
+        assertEquals(parkingLot2.getAvailableParkingPosition(), 0);
+        assertEquals(parkingLot3.getAvailableParkingPosition(), 9);
+    }
+
+
 }
