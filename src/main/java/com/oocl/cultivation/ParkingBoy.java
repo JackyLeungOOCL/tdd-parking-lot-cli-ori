56cf6eeafe12b8f613e6f0f5bbb104e0185caf9b
jacky.leung <jacky.leung@oocl.com>
2018-11-27 01:00:37 +0800

    [LEUNGJA] update: parking boy refactor

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingBoy {
-
-//    private final ParkingLot parkingLot;
     protected List<ParkingLot> parkingLots;
     protected String lastErrorMessage;
 
@@ -19,20 +17,7 @@ public class ParkingBoy {
     }
 
     public ParkingTicket park(Car car) {
-        // TODO: Please implement the method
         return park(car, 0);
-//        try {
-//            int parkingLotNumber = 0;
-//            ParkingTicket ticket = parkingLot.generateTicket(car);
-//            lastErrorMessage = null;
-//            return ticket;
-//        } catch (RuntimeException e) {
-//            lastErrorMessage = e.getMessage();
-//            if (lastErrorMessage == "The parking lot is full.") {
-//
-//            }
-//            return null;
-//        }
     }
 
     public ParkingTicket park(Car car, int parkingLotNumber) {
@@ -45,25 +30,19 @@ public class ParkingBoy {
             return ticket;
         } catch (RuntimeException e) {
             lastErrorMessage = e.getMessage();
-//            return null;
-            if (lastErrorMessage == "The parking lot is full.") {
+            if (isParkingLotFullError()) {
                 return park(car, ++parkingLotNumber);
             }
             return null;
         }
     }
 
+    private boolean isParkingLotFullError() {
+        return lastErrorMessage.equals("The parking lot is full.");
+    }
+
     public Car fetch(ParkingTicket ticket) {
-        // TODO: Please implement the method
         return fetch(ticket, 0);
-//        try {
-//            Car car = parkingLot.receiveTicket(ticket);
-//            lastErrorMessage = null;
-//            return car;
-//        } catch (RuntimeException e) {
-//            lastErrorMessage = e.getMessage();
-//            return null;
-//        }
     }
 
     public Car fetch(ParkingTicket ticket, int parkingLotNumber) {
@@ -76,11 +55,15 @@ public class ParkingBoy {
             return car;
         } catch (RuntimeException e) {
             lastErrorMessage = e.getMessage();
-            if (lastErrorMessage == "Unrecognized parking ticket.") {
+            if (isUnrecognizedTicketError()) {
                 return fetch(ticket, ++parkingLotNumber);
             }
             return null;
         }
+    }
+
+    private boolean isUnrecognizedTicketError() {
+        return lastErrorMessage.equals("Unrecognized parking ticket.");
     }
 
     public String getLastErrorMessage() {
